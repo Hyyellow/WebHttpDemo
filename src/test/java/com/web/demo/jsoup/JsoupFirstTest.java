2f8e4087f43c016954d8e23cb8c48ffa0d81fea5
YaYaHuang <570911275@qq.com>
2020-03-25 07:29:33 +0800

    根据选择器获取元素

--- src/test/java/com/web/demo/jsoup/JsoupFirstTest.java
+++ src/test/java/com/web/demo/jsoup/JsoupFirstTest.java
@@ -118,7 +118,42 @@ public class JsoupFirstTest {
 
         // 5.从元素中获取文本内容test
         str = element.text();
-        
+
         System.out.println("获取到的元素内容是"+str);
     }
+
+    @Test
+    /*
+     * 根据选择器获取元素
+     * */
+    public void testSelector() throws IOException {
+        // 解析文件
+        Document doc = Jsoup.parse(new File("F:\\Work\\JsoupStringTest.html"),"utf8");
+
+        // 获取元素
+
+        // 1.根据标签查找元素
+        Elements span = doc.select("span");
+        /*for (Element element : span) {
+            System.out.println(element.text());
+        }*/
+
+        // 2.根据id查找元素 #id
+        // Element element = doc.select("#city_bj").first();
+
+        // 3.根据class查找元素 .class
+        // Element element = doc.select(".class_a").first();
+
+        // 4.根据属性查找元素 [abc]
+        Element element = doc.select("[abc]").first();
+
+        // 5.根据属性值查找元素 [class=s_name]
+        Elements select = doc.select("[class=s_name]");
+        for (Element element1:select) {
+            System.out.println(element.text());
+        }
+
+        System.out.println("获取到的结果"+element.text());
+    }
+
 }
